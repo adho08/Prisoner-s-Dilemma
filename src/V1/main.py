fd132af832ed278ca0e5e8e2e7ca3027038c1509
Adrian Hossner <adhos@AdriansLaptop.>
2025-04-14 23:59:00 +0200

    Basic single PD

--- src/V1/main.py
+++
@@ -1,5 +0,0 @@
-# import strategies from strategies directory
-
-# import strategies from strategies directory
-
-# initialize the game# initialize the game
---
+++ src/main.py
@@ -0,0 +1,30 @@
+# import strategies from strategies directory
+from strategies import Cooperate, Defect
+from game import PrisonersDilemma
+
+# initialize the strategies
+AC = Cooperate()
+AD = Defect()
+
+# initialize the game
+PD = PrisonersDilemma(AC, AC)
+ 
+def main():
+    s1 = PD.strategy1
+    s2 = PD.strategy2
+
+    m1 = s1.make_move()
+    m2 = s2.make_move()
+    result = PD.award(m1, m2)
+
+
+    # printing the result
+
+    padding = 14
+    print(f"{s1.name : <{padding}} | {s2.name : <{padding}}")
+    print((2 * padding + 2) * '-')
+    print(f"{result[0] : <{padding}} | {result[1] : <{padding}}")
+
+if __name__ == '__main__':
+    main()
+
