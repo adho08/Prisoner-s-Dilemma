1b3a5313d97b5a99e7aee0bb8176ebe2689fe964
Adrian Hossner <adhos@AdriansLaptop.>
2025-05-26 14:30:59 +0200

    structuring IPD

---
+++ src/IPD/strategies.py
@@ -0,0 +1,48 @@
+import random
+
+class Strategy:
+    def __init__(self, start, name):
+        self.start = start
+        self.name = name
+        self.history = []
+        self.opponent_history = []
+        self.points = 0
+
+    def make_move(self, round):
+        # Return True for 'c' or False for 'd'
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def update(self, move, opponent_move):
+        self.history.append(move)
+        self.opponent_history.append(opponent_move)
+
+class AlwaysCooperate(Strategy):
+    def __init__(self):
+        super().__init__(True, "AlwaysCooperate")
+
+    def make_move(self, round):
+        return True
+
+class AlwaysDefect(Strategy):
+    def __init__(self):
+        super().__init__(False, "AlwaysDefect")
+
+    def make_move(self, round):
+        return False
+
+class Tit4Tat(Strategy):
+    def __init__(self):
+        super().__init__(True, "TitForTat")
+
+    def make_move(self, round):
+        if round == 0:
+            return self.start
+        else:
+            return self.opponent_history[round - 1]
+
+class Random(Strategy):
+    def __init__(self):
+        super().__init__(True, "Random")
+
+    def make_move(self, round):
+        return random.choice([True, False])
