9281ed963a59ef4b2cf09492443f79b5d3431460
Adrian Hossner <adrianho@fedora.home>
2025-07-04 22:00:10 +0200

    surface plotting

--- VBSA/data/surface.py
+++ VBSA/data/surface.py
@@ -1,25 +1,39 @@
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import pandas as pd
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 
-# Create data
-x = np.linspace(-5, 5, 50)
-y = np.linspace(-5, 5, 50)
-X, Y = np.meshgrid(x, y)
-Z = np.sin(np.sqrt(X**2 + Y**2))
+df = pd.read_csv("results.csv")
 
-# Create the plot
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
+# parameter axes
+list1 = df[df.columns[0]].to_numpy().tolist()
+list2 = df[df.columns[2]].to_numpy().tolist()
 
-# Create surface plot
-surface = ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
+# make elements unique
+list1 = list(set(list1))
+list2 = list(set(list2))
 
-# Add labels
-ax.set_xlabel('X axis')
-ax.set_ylabel('Y axis')
-ax.set_zlabel('Z axis')
-ax.set_title('3D Surface Plot')
+n, m = len(list1), len(list2)
 
-# Add colorbar
-fig.colorbar(surface)
+list3 = df[df.columns[1]]
+matrix3 = np.array(list3).reshape((n, m))
+
+
+fig = go.Figure(go.Surface(
+    # contours = {
+    #     "x": {"show": True, "start": 1.5, "end": 2, "size": 0.04, "color":"white"},
+    #     "z": {"show": True, "start": 0.5, "end": 0.8, "size": 0.05}
+    # },
+    x = list1,
+    y = list2,
+    z = matrix3
+    ))
+fig.update_layout(
+        scene = dict(
+            xaxis = dict(nticks=4, range=[0,10],),
+            yaxis = dict(nticks=4, range=[0,10],),
+            zaxis = dict(nticks=4, range=[0,100],),),
+        width=700, height = 700,
+        margin=dict(r=20, l=10, b=10, t=10)
+        )
+
+fig.show()
