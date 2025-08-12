7f385a93d573ef79a99c26c2fdd301be6ab2301b
Adrian Hossner <adrianho@fedora.home>
2025-08-12 23:05:06 +0200

    adding Adapt4

--- VBSA/src/strategies/Others.py
+++ VBSA/src/strategies/Others.py
@@ -1,4 +1,5 @@
 from .strategies import Strategy, PBStrategy
+import builtins
 
 class Inverse(Strategy):
     def __init__(self, name: str | None = None):
@@ -38,6 +39,29 @@ class Adapt(PBStrategy):
             return max(0.0, min(1.0, self._history[-1] - self.parameter/10))
 
 class Adapt2(PBStrategy):
+    def __init__(self, parameter: int = 1, start = 0.5, name: str | None = None):
+        super().__init__(parameter, start, name)
+        self._retaliates = True
+        self._isForgiving = True
+        self._isEnvious = False
+
+    def make_move(self, round=None) -> float:
+        """
+        Starts the first two rounds with full cooperation.
+        If the opponent defected more than Adapt, defect parameter/10 more in the next round.
+        If the opponent cooperated more than Adapt, go the that level of cooperation in the next round.
+        Parameter 0 is identical to AlwaysCooperate. (See Basic.py)
+        """
+        if round < 2 and self._start != None:
+            return self._start
+        # if opponent's move is greater than the last own move, cooperate parameter/10 more than last round 
+        elif self._opponent_history[-1] >= self._history[-1]:
+            return max(0.0, min(1.0, self._history[-1] + self.parameter/10))
+        # else defect parameter/10 more
+        else:
+            return max(0.0, min(1.0, self._history[-1] - self.parameter/10))
+
+class Adapt3(PBStrategy):
     def __init__(self, parameter: int = 1, start = 1.0, name: str | None = None):
         super().__init__(parameter, start, name)
         self._retaliates = True
@@ -48,14 +72,35 @@ class Adapt2(PBStrategy):
         """
         Starts the first two rounds with full cooperation.
         If the opponent defected more than Adapt, defect parameter/10 more in the next round.
-        If the opponen cooperated more than Adapt, go the that level of cooperation in the next round.
+        If the opponent cooperated more than Adapt, go the that level of cooperation in the next round.
         Parameter 0 is identical to AlwaysCooperate. (See Basic.py)
         """
         if round < 2 and self._start != None:
             return self._start
         # if opponent's move is greater than the last own move, go to his level of cooperation
         elif self._opponent_history[-1] >= self._history[-1]:
-            return max(0.0, min(1.0, self._history[-1] + self.parameter/10))
+            print(self._history[-1] + self.parameter/10, builtins.round(max(0.0, min(1.0, self._history[-1] + self.parameter/10))))
+            return builtins.round(self._history[-1] + self.parameter/10)
         # else defect parameter/10 more
         else:
-            return max(0.0, min(1.0, self._history[-1] - self.parameter/10))
+            print(builtins.round(self._history[-1] - self.parameter/10))
+            return builtins.round(self._history[-1] - self.parameter/10)
+
+class Adapt4(PBStrategy):
+    def __init__(self, parameter: int = 1, start = 1.0, name: str | None = None):
+        super().__init__(parameter, start, name)
+        self._retaliates = True
+        self._isForgiving = True
+        self._isEnvious = False
+
+    def make_move(self, round=None) -> float:
+        """
+        Start first round with full cooperation.
+        It takes the difference between the two contributions and adds that difference to its previous contribution.
+        This shift is the difference multiplied by the parameter/5.
+        Parameter 0 is AlwaysCooperate and parameter 10 will ...
+        """
+        if round <= 1 and self._start != None:
+            return self._start
+        else:
+            return max(0.0, min(1.0, self._history[-1] + self.parameter/5 * (self._opponent_history[-1] - self._history[-1])))
