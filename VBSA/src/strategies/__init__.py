eadb9570e916774e2c6c40d453bda1f42fdb3048
Adrian Hossner <adrianho@fedora.home>
2025-08-12 23:04:44 +0200

    including new implemented strategies

--- VBSA/src/strategies/__init__.py
+++ VBSA/src/strategies/__init__.py
@@ -1,6 +1,6 @@
 from .strategies import Strategy, PBStrategy
-from .Basic import AlwaysCooperate, AlwaysDefect, Neutral
+from .Basic import AlwaysCooperate, AlwaysDefect, Neutral, AlwaysSame
 from .Average import Average
-from .Others import Adapt, Inverse, Adapt2
+from .Others import Adapt, Inverse, Adapt2, Adapt3, Adapt4
 from .Random import RandomDiscrete, RandomNeutral
 from .TitForTat import Tit4Tat, Tit4TatB
