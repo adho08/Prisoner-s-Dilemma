cc77263257624630de20426abc0f58e46ec48d36
Adrian Hossner <adrianho@fedora.home>
2025-08-23 15:31:05 +0200

    backup

--- VBSA/data/AdaptC_vs_AlwaysS.py
+++ VBSA/data/AdaptC_vs_AlwaysS.py
@@ -1,4 +1,7 @@
+import csv
+
 payoffs_A = []
+investments = []
 
 def payoff(x, y, c=0.5) -> tuple[float, float]:
 
@@ -18,6 +21,7 @@ def i(p_A, p_R, r, print_r=None):
     p,_ = payoff(investm, p_R/10)
     if print_r:
         payoffs_A.append(p)
+        investments.append(investm)
         print(f"{r}. round:\t {investm:.2f},\t {p}")
     return investm, 2, p
 
@@ -28,6 +32,19 @@ def s(p_A, p_R, r):
 def print_i(p_A, p_R, r):
     i(p_A, p_R, r, r)
 
-print_i(2, 0, 20)
+rounds = 20
+parameter_A = 10
+parameter_B = 0
+print_i(parameter_A, parameter_B, rounds)
 
 print(sum(payoffs_A))
+
+with open(f'AdaptC_AlwaysS_simulation/AdaptC_vs_AlwaysS_simulation_{parameter_A}_{parameter_B}.csv', 'w', newline='') as csvfile:
+    fieldnames = ['round', 'investment', 'payoff']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for index in range(rounds):
+        writer.writerow({'round': index+1, 'investment': investments[index], 'payoff': payoffs_A[index]})
+
+
---
+++ VBSA/data/generate_image_AdaptC_AlwaysS.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+# Build figure
+fig = go.Figure()
+
+def add_trace_to_fig(fig, csv_file, i):
+    df = pd.read_csv(csv_file)
+    fig.add_trace(
+        go.Scatter(
+            x=df[df.columns[0]], 
+            y=df[df.columns[1]], 
+            name=f"Parameter {i}"
+        )
+    )
+
+directory = "AdaptC_AlwaysS_simulation"
+parameter_B = 5
+parameter_start = 5
+parameter_end = 10
+list_parameters = range(parameter_start, parameter_end+1)
+for i in list_parameters:
+    add_trace_to_fig(fig, f"{directory}/AdaptC_vs_AlwaysS_simulation_{i}_{parameter_B}.csv", i)
+
+fig.update_layout(
+    xaxis=dict(range=[1, 20], title="Round", dtick=1),
+    yaxis=dict(range=[0, 1], title="Adapt-Continuous's Invesment", dtick=0.1
+    )
+)
+
+fig.write_image(f"plots/Discussion/Adapt-Continuous/Approx_{parameter_B}_line_chart_{parameter_start}-{parameter_end}.png")
+
